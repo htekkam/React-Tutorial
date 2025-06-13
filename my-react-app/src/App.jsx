79f6c5f2369ba5381450995f1fa35ef5cd4c0d65
harikrishna t <harikrishna.tekkam@gmail.com>
2025-06-13 10:34:46 +0530

    removing temp files

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import './App.css' 
+
+let a = "Thriller"
+let b = "Suspense"
+
+function App() {
+  return (
+     <div className='container'>
+     <h1>I like {a} and {b} movies</h1>
+      <p className='para'>This is a paragraph</p>
+    </div>
+  )
+}
+
+export default App
